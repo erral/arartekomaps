587b3936f4ceb8d5bf3ef26d9d443e6869c90739
urtzai <uodriozola@codesyntax.com>
2012-11-12 13:40:11 +0100

    Ararteko search and image list style

--- arartekomaps/settings.py
+++ arartekomaps/settings.py
@@ -18,12 +18,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'arartekomaps',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'pos$
+        'NAME': 'arartekomaps',                      # Or path to database file$
+        'USER': 'artkmaps_user',                      # Not used with sqlite3.
+        'PASSWORD': 'fxXYIH1EHy',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. N$
+        'PORT': '',                      # Set to empty string for default. Not$
     }
 }
 
